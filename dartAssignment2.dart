b242dbd7d291067ed35dc0ff71669408236e8a19
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-10-17 00:23:38 +0600

    fixed some indention error

--- dartAssignment2.dart
+++ dartAssignment2.dart
@@ -17,96 +17,90 @@ void checkOddEven(int number) {
 // 2. Write a dart program to check whether a character is a vowel or consonant.
 
 void checkVowelConsonant(String char) {
- 
-  String lowerChar = char.toLowerCase();
-  
-
-
-  
-  if (lowerChar == 'a' || lowerChar == 'e' || lowerChar == 'i' || lowerChar == 'o' || lowerChar == 'u') {
-    print('$char is a Vowel.');
-  } else {
-    print('$char is a Consonant.');
-  }
+  String lowerChar = char.toLowerCase();
+  
+  if (lowerChar == 'a' || lowerChar == 'e' || lowerChar == 'i' || lowerChar == 'o' || lowerChar == 'u') {
+    print('$char is a Vowel.');
+  } else {
+    print('$char is a Consonant.');
+  }
 }
-
 
 // 3. Write a dart program to check whether a number is positive, negative, or zero.
 void checkPosNegZero(num number) {
-  if (number > 0) {
-    print('$number is Positive.');
-  } else if (number < 0) {
-    print('$number is Negative.');
-  } else {
-    print('$number is Zero.');
-  }
+  if (number > 0) {
+    print('$number is Positive.');
+  } else if (number < 0) {
+    print('$number is Negative.');
+  } else {
+    print('$number is Zero.');
+  }
 }
 
 
 // 4. Write a dart program to print your name 100 times.
 void printName100Times(String name) {
-  for (int i = 0; i < 100; i++) {
-    print('${i + 1}. $name');
-  }
+  for (int i = 0; i < 100; i++) {
+    print('${i + 1}. $name');
+  }
 }
-
 
 // 5. Write a dart program to calculate the sum of natural numbers.
 void sumOfNaturalNumbers(int n) {
-  int sum = 0;
-  for (int i = 1; i <= n; i++) {
-    sum += i;
-  }
-  print('The sum of natural numbers up to $n is: $sum');
+  int sum = 0;
+  for (int i = 1; i <= n; i++) {
+    sum += i;
+  }
+  print('The sum of natural numbers up to $n is: $sum');
 }
 
 
 // 6. Write a dart program to generate multiplication tables of 5.
 void multiplicationTableOf5() {
-  int number = 5;
-  for (int i = 1; i <= 10; i++) {
-    print('$number x $i = ${number * i}');
-  }
+  int number = 5;
+  for (int i = 1; i <= 10; i++) {
+    print('$number x $i = ${number * i}');
+  }
 }
 
 
 // 7. Write a dart program to generate multiplication tables of 1-9.
 void multiplicationTables1to9() {
-  for (int table = 1; table <= 9; table++) {
-    print('\n--- Multiplication Table of $table ---');
-    for (int i = 1; i <= 10; i++) {
-      print('$table x $i = ${table * i}');
-    }
-  }
+  for (int table = 1; table <= 9; table++) {
+    print('\n--- Multiplication Table of $table ---');
+    for (int i = 1; i <= 10; i++) {
+      print('$table x $i = ${table * i}');
+    }
+  }
 }
 
 // 8. Write a dart program to create a simple calculator that performs addition, subtraction, multiplication, and division.
 void simpleCalculator(double num1, double num2, String operation) {
-  double result;
-  switch (operation) {
-    case '+':
-      result = num1 + num2;
-      print('$num1 + $num2 = $result');
-      break;
-    case '-':
-      result = num1 - num2;
-      print('$num1 - $num2 = $result');
-      break;
-    case '*':
-      result = num1 * num2;
-      print('$num1 * $num2 = $result');
-      break;
-    case '/':
-      if (num2 != 0) {
-        result = num1 / num2;
-        print('$num1 / $num2 = $result');
-      } else {
-        print('Error: Division by zero is not allowed.');
-      }
-      break;
-    default:
-      print('Invalid operation specified.');
-  }
+  double result;
+  switch (operation) {
+    case '+':
+      result = num1 + num2;
+      print('$num1 + $num2 = $result');
+      break;
+    case '-':
+      result = num1 - num2;
+      print('$num1 - $num2 = $result');
+      break;
+    case '*':
+      result = num1 * num2;
+      print('$num1 * $num2 = $result');
+      break;
+    case '/':
+      if (num2 != 0) {
+        result = num1 / num2;
+        print('$num1 / $num2 = $result');
+      } else {
+        print('Error: Division by zero is not allowed.');
+      }
+      break;
+    default:
+      print('Invalid operation specified.');
+  }
 }
 
 
