5821f55f83edd388286a95ca3b301876cb8fe979
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-10-17 00:07:22 +0600

    generating random password

--- dartAssignment2.dart
+++ dartAssignment2.dart
@@ -144,3 +144,18 @@ void printEvenNumbersInInterval(int start, int end) {
 void greet(String name) {
   print('Hello, $name.');
 }
+
+// Problem 13: Generate Random Password
+
+String generateRandomPassword({int length = 12}) {
+  const String chars = 
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#\$%^&*()_+-=';
+  Random random = Random();
+  String password = '';
+
+  for (int i = 0; i < length; i++) {
+    int randomIndex = random.nextInt(chars.length);
+    password += chars[randomIndex];
+  }
+  return password;
+}
