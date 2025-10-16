5cd81a153c7ceaa06c54742ebe07305a7d26c337
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-10-16 23:50:31 +0600

    check vowel or consonant

--- dartAssignment2.dart
+++ dartAssignment2.dart
@@ -13,3 +13,19 @@ void checkOddEven(int number) {
     print('$number is Odd.');
   }
 }
+
+// 2. Write a dart program to check whether a character is a vowel or consonant.
+
+void checkVowelConsonant(String char) {
+ 
+  String lowerChar = char.toLowerCase();
+  
+
+
+  
+  if (lowerChar == 'a' || lowerChar == 'e' || lowerChar == 'i' || lowerChar == 'o' || lowerChar == 'u') {
+    print('$char is a Vowel.');
+  } else {
+    print('$char is a Consonant.');
+  }
+}
