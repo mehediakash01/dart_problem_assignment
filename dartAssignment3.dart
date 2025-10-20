ad1d52ae904a44e8110641eb0b9f35d78a5ac914
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-10-20 19:04:26 +0600

    created animal class and extened it further

--- dartAssignment3.dart
+++ dartAssignment3.dart
@@ -60,3 +60,28 @@ void main() {
     print(g);
   }
 }
+
+//4. Write a dart program to create a class Animal with properties [id, name, color]. Create another class called Cat and extends it from Animal. Add new property sound in String. Create an object of a Cat and print all details.
+class Animal {
+  int id;
+  String name;
+  String color;
+
+  Animal(this.id, this.name, this.color);
+}
+
+class Cat extends Animal {
+  String sound;
+
+  Cat(int id, String name, String color, this.sound) : super(id, name, color);
+
+  void showDetails() {
+    print('ID: $id, Name: $name, Color: $color, Sound: $sound');
+  }
+}
+
+void main() {
+  var cat = Cat(1, 'Tom', 'Gray', 'Meow');
+  print('--- Cat Details ---');
+  cat.showDetails();
+}
