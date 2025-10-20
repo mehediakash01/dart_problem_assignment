09bd0c45290111aba2e3bcbf1d1039f85fadc3ec
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-10-20 19:07:10 +0600

    created private class and then create getter and setter to get and set values

--- dartAssignment3.dart
+++ dartAssignment3.dart
@@ -85,3 +85,35 @@ void main() {
   print('--- Cat Details ---');
   cat.showDetails();
 }
+
+//5. Write a dart program to create a class Camera with private properties [id, brand, color, price]. Create a getter and setter to get and set values. Also, create 3 objects of it and print all details.
+class Camera {
+  int _id;
+  String _brand;
+  String _color;
+  double _price;
+
+  Camera(this._id, this._brand, this._color, this._price);
+
+  int get id => _id;
+  String get brand => _brand;
+  String get color => _color;
+  double get price => _price;
+
+  set price(double newPrice) => _price = newPrice;
+
+  void display() {
+    print('ID: $_id, Brand: $_brand, Color: $_color, Price: \$$_price');
+  }
+}
+
+void main() {
+  var cam1 = Camera(1, 'Canon', 'Black', 1200);
+  var cam2 = Camera(2, 'Nikon', 'Silver', 1500);
+  var cam3 = Camera(3, 'Sony', 'Gray', 2000);
+
+  print('--- Camera Details ---');
+  cam1.display();
+  cam2.display();
+  cam3.display();
+}
