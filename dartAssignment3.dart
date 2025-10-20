238c96425c57ab8602958ac0201c1bb8475d8e11
ronSiuuuu <mehedihasanakash1717@gmail.com>
2025-10-20 19:00:14 +0600

    created house class and object of it and then added them in list and print afterall

--- dartAssignment3.dart
+++ dartAssignment3.dart
@@ -1,4 +1,4 @@
-// Write a dart program to create a class Laptop with properties [id, name, ram] and create 3 objects of it and print all details.
+//1. Write a dart program to create a class Laptop with properties [id, name, ram] and create 3 objects of it and print all details.
 class Laptop {
   int id;
   String name;
@@ -20,4 +20,32 @@ void main() {
   laptop1.display();
   laptop2.display();
   laptop3.display();
+}
+
+// 2. Write a dart program to create a class House with properties [id, name, price]. Create a constructor of it and create 3 objects of it. Add them to the list and print all details
+
+
+class House {
+  int id;
+  String name;
+  double price;
+
+  House(this.id, this.name, this.price);
+
+  void show() {
+    print('House ID: $id, Name: $name, Price: \$${price.toStringAsFixed(2)}');
+  }
+}
+
+void main() {
+  var houseList = [
+    House(1, 'Green Villa', 250000),
+    House(2, 'Ocean View', 500000),
+    House(3, 'Mountain Nest', 300000),
+  ];
+
+  print('--- House Details ---');
+  for (var house in houseList) {
+    house.show();
+  }
 }
